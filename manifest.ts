7420b503b146aaf9ab740666489a17789b610c94
James <jamesarnott2@gmail.com>
2022-07-11 23:39:07 +0100

    chore: committing manifest

--- manifest.ts
+++ manifest.ts
@@ -1,6 +1,47 @@
 export default {
   "manifest_version": 3,
-  "name": "CRXJS React Vite Example",
-  "version": "1.0.0",
-  "action": { "default_popup": "index.html" }
+  "name": "LighterFuel For Tinder",
+  "short_name": "LighterFuel",
+  "description": "Used to get the time of when profile images are uploaded to tinder",
+  "version": "1.3",
+  "action": {
+    "default_title": "LighterFuel",
+    "default_icon": "src/assets/LighterFuel64.png",
+    "default_popup": "index.html"
+  },
+  "icons": {
+    "64": "LighterFuel64.png"
+  },
+  "externally_connectable": {
+    "matches": ["*://tinder.com/*"]
+  },
+  "permissions": [
+    "webRequest",
+    "storage",
+    "scripting"
+  ],
+  "host_permissions": [
+    "*://*.tinder.com/*", 
+    "*://*.gotinder.com/*"
+  ],
+  "content_scripts": [
+    {
+      "matches": ["*://*.tinder.com/*"],
+      "js": ["./static/js/LighterFuel.js"],
+      "css": ["./injectedCss.css"],
+      "all_frames": false,
+      "run_at": "document_end"
+    }
+  ],
+  "background": {
+    "service_worker": "/static/js/background.js"
+  },
+  "web_accessible_resources": [{
+    "resources": [
+      "static/*", 
+      "./injectedCss.css"
+    ],
+    "matches": ["<all_urls>"],
+    "use_dynamic_url": true
+  }]
 }
