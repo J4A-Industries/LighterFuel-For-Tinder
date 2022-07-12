f0aff05a8895e17f569c53b67759d395b797ee93
James <jamesarnott2@gmail.com>
2022-07-13 00:53:32 +0100

    fix: clean up of basic misc functions

--- src/background/injected/Misc.ts
+++ src/background/injected/Misc.ts
@@ -1,9 +1,22 @@
+import {debug} from '@/config';
+
+/**
+ * Transfers the input image to the "full quality" tinder ones
+ * Highly likely to break soon
+ *
+ * @param {String} url
+ * @returns {String}
+ */
+ export const getFullQualityImage = (url: string) => {
+  return url.replace(url.split('/')[4].split('_')[0], 'original').replace('.jpg', '.jpeg');
+};
+
 /**
    * Used to genereate the buttons
    *
    * @returns {HTMLElement} The buttons
    */
- export const createButtons = (data) => {
+ export const createButtons = (data: {url: string}) => {
   const parent = document.createElement('div');
   parent.classList.add('buttonParent');
   // * URLs that start with ... are private (their URL can't be passed to any other service)
@@ -11,12 +24,15 @@
   // * Let's just only allow this on matches for now
   if (!data.url.startsWith('https://images-ssl.gotinder.com/u/')) {
     const searchButton = document.createElement('div');
+    const enlargedUrl = getFullQualityImage(data.url);
+    const reverseImageUrl = `https://www.bing.com/images/search?view=detailv2&iss=sbi&form=SBIIDP&sbisrc=UrlPaste&q=imgurl:${encodeURIComponent(data.url)}&exph=800&expw=640&vt=2&sim=15`;
     searchButton.classList.add('buttonLF');
     searchButton.classList.add('search');
     searchButton.innerText = 'Search';
     searchButton.onclick = () => {
-      console.log(`https://www.bing.com/images/search?view=detailv2&iss=sbi&form=SBIIDP&sbisrc=UrlPaste&q=imgurl:${encodeURIComponent(data.url)}&exph=800&expw=640&vt=2&sim=15`);
-      window.open(`https://www.bing.com/images/search?view=detailv2&iss=sbi&form=SBIIDP&sbisrc=UrlPaste&q=imgurl:${encodeURIComponent(data.url)}&exph=800&expw=640&vt=2&sim=15`, '_blank').focus();
+      if(debug) console.log('Searching for', reverseImageUrl);
+      const newTab = window.open(reverseImageUrl, '_blank');
+      if(newTab) newTab.focus();
     };
     parent.appendChild(searchButton);
 
@@ -25,23 +41,12 @@
     enlargeButton.classList.add('enlarge');
     enlargeButton.innerText = 'Enlarge';
     enlargeButton.onclick = () => {
-      window.open(this.getFullQualityImage(data.url), '_blank').focus();
+      const newTab = window.open(enlargedUrl, '_blank');
+      if(newTab) newTab.focus();
     };
     parent.appendChild(enlargeButton);
   }
   return parent;
-};
-
-/**
- * TODO: move this to an external helper file
- * Transfers the input image to the "full quality" tinder ones
- * Highly likely to break soon
- *
- * @param {String} url
- * @returns {String}
- */
-export const getFullQualityImage = (url) => {
-  return url.replace(url.split('/')[4].split('_')[0], 'original').replace('.jpg', '.jpeg');
 };
 
 /**
@@ -50,12 +55,13 @@ export const getFullQualityImage = (url) => {
  * @param {String} time The Date.toString() time, eg "Wed, 08 Apr 2020 22:14:00 GMT"
  * @returns {String} How many days/weeks/years ago, eg "20 Days"
  */
-export const getTimeOld = (time) => {
+export const getTimeOld = (time: number) => {
   // get days old
   const days = Math.round((Date.now() - time) / 24 / 60 / 60 / 1000);
   if (days / 365 > 1) {
     return `${Math.round((days / 365) * 100) / 100} Years`;
-  } if (days / 7 > 1) {
+  } 
+  if (days / 7 > 1) {
     return `${Math.round((days / 7) * 10) / 10} Weeks`;
   }
   return `${days} Days`;
@@ -68,7 +74,7 @@ export const getTimeOld = (time) => {
  * @param {Array} urlArray The array of URLs that the method searches for
  * @returns {{domNode: DomNode, data: Object}[]} An array of images found with the node and the data entry
  */
- export const getProfileImages = (doc, urlArray) => {
+ export const getProfileImages = (doc: { querySelectorAll: (arg0: string) => Iterable<unknown> | ArrayLike<unknown>; }, urlArray: any[]) => {
   if (!doc) return [];
   // The regex to check for the background to match `url(...)`
   const srcChecker = /url\(\s*?['"]?\s*?(\S+?)\s*?["']?\s*?\)/i;
@@ -81,7 +87,7 @@ export const getTimeOld = (time) => {
         const match = srcChecker.exec(prop);
         if (match) {
           // look for the found URL in the URL list
-          const urlEntry = urlArray.find((x) => x.url === match[1]);
+          const urlEntry = urlArray.find((x: { url: string; }) => x.url === match[1]);
           // if the URL is in the list and the node has the classes 'StretchedBox' or 'profileCard__slider__img'
           if (urlEntry && (node.classList.contains('StretchedBox') || node.classList.contains('profileCard__slider__img'))) {
             // add tothe collection
@@ -95,12 +101,11 @@ export const getTimeOld = (time) => {
 
 /**
  * Probably could be obsolete and replaced with getProfileImages, will remove in the future
- * TODO: move this to an external helper file
  *
  * @param {HTMLElement} doc The document to search though
  * @returns {{HTMLElement}[]} The array of nodes
  */
-export const getDomsWithBGImages = (doc) => {
+export const getDomsWithBGImages = (doc: { querySelectorAll: (arg0: string) => Iterable<unknown> | ArrayLike<unknown>; }) => {
   if (!doc) return [];
   const srcChecker = /url\(\s*?['"]?\s*?(\S+?)\s*?["']?\s*?\)/i;
   return Array.from(
@@ -128,7 +133,7 @@ export const getDomsWithBGImages = (doc) => {
  * @param {HTMLElement} node
  * @returns {String} the URL from the node
  */
- export const getImageURLfromNode = (node) => {
+ export const getImageURLfromNode = (node: Element) => {
   const srcChecker = /url\(\s*?['"]?\s*?(\S+?)\s*?["']?\s*?\)/i;
   // get background image from node
   const prop = window.getComputedStyle(node, null).getPropertyValue('background-image');
