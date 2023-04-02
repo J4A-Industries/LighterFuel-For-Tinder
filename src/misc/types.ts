eb5986c54e1d0615591b0f27a52209a72d114383
James <jamesarnott2@gmail.com>
2023-04-02 19:12:15 +0100

    chore: moving files over to the new folder structure

---
+++ src/misc/types.ts
@@ -0,0 +1,26 @@
+export type ImageType = {
+  url: string;
+  lastModified: string;
+  timeAddedToArr: Date;
+}
+
+export type ProfileImage = {
+  domNode: HTMLElement;
+  data: ImageType;
+}
+
+export type ShowSettings = {
+  overlayButton: boolean;
+  searchButton: boolean;
+};
+
+export type TabMessage = {
+  action: string;
+  data: ImageType | ShowSettings;
+};
+
+export type AISettings = {
+	temperature: number;
+	bestOf: number;
+	APIkey: string;
+};
