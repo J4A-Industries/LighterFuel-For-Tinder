fb45ebb784ebb2436fdfc1a1fbf147cea5b59177
James <jamesarnott2@gmail.com>
2023-04-03 16:55:42 +0100

    chore: misc changes

--- src/contents/index.ts
+++ src/contents/index.ts
@@ -3,22 +3,22 @@ import styleText from 'data-text:./injectedCss.css';
 import LighterFuel from './LighterFuel';
 import { debug } from '@/misc/config';
 
+export const config: PlasmoCSConfig = {
+  matches: ['https://tinder.com/*'],
+  run_at: 'document_start',
+};
+
 export const getStyle: PlasmoGetStyle = () => {
   const style = document.createElement('style');
   style.textContent = styleText;
   return style;
 };
 
-export const config: PlasmoCSConfig = {
-  matches: ['https://www.tinder.com/*'],
-};
-
-window.addEventListener('load', () => {
-  try {
-    const lf = new LighterFuel();
-    // prints the lf instance to the console for debugging!
-    if (debug) console.log(lf);
-  } catch (err) {
-    console.error(err);
-  }
-});
+try {
+  console.log('Working!');
+  const lf = new LighterFuel();
+  // prints the lf instance to the console for debugging!
+  if (debug) console.log(lf);
+} catch (err) {
+  console.error(err);
+}
