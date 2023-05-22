10beb81e1a5bdae2dc8988e70ddd52da79584da1
James <jamesarnott2@gmail.com>
2023-05-23 00:11:52 +0100

    feat: added sending arrays of ImageType objects to webpage

--- src/background/Misc.ts
+++ src/background/Misc.ts
@@ -6,14 +6,15 @@ import { debug } from '@/misc/config';
    * This filters through all the tabs and sends the info to them
    *
    */
-export const sendImageDataToTab = (obj: ImageType) => new Promise<void>((resolve) => {
+export const sendImageDataToTab = (obj: ImageType | ImageType[]) => new Promise<void>((resolve) => {
+  const arrIn = Array.isArray(obj);
   browser.tabs.query({})// query all tabs
     .then((x) => x.filter((y) => y.url))// filter to only ones we have permission to look at
     .then((tabs) => {
       tabs.forEach((tab) => {
         if (tab.id) {
           const msg: TabMessage = {
-            action: 'new image',
+            action: arrIn ? 'new images' : 'new image',
             data: obj,
           };
           browser.tabs.sendMessage(tab.id, msg).catch((e) => {
