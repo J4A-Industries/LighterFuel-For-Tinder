a52f4084f52e4ca9ad28ef4664cad60a60a5ac8d
James <jamesarnott2@gmail.com>
2023-05-23 14:54:38 +0100

    feat: created messaging endpoint for getting all images in cache

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-mutable-exports */
 import browser from 'webextension-polyfill';
 import { Storage } from '@plasmohq/storage';
 import { debug, defaultSettings } from '@/misc/config';
@@ -13,13 +14,21 @@ const setDefaultSettings = async () => {
   }
 };
 
+let tinderRequestCap: ImageRequestCapturer;
+let mambaRequestCap: ImageRequestCapturer;
+
 try {
   setDefaultSettings();
-  const tinderRequestCap = new ImageRequestCapturer(['*://*.gotinder.com/*/*.jpg*', '*://*.gotinder.com/*/*.webp*', '*://*.gotinder.com/*/*.mp4*'], Sites.TINDER);
-  const mambaRequestCap = new ImageRequestCapturer(['*://*.wmbcdn.com/*'], Sites.MAMBA, 1000);
+  tinderRequestCap = new ImageRequestCapturer(['*://*.gotinder.com/*/*.jpg*', '*://*.gotinder.com/*/*.webp*', '*://*.gotinder.com/*/*.mp4*'], Sites.TINDER);
+  mambaRequestCap = new ImageRequestCapturer(['*://*.wmbcdn.com/*'], Sites.MAMBA, 1000);
 
   // prints the bg instance to the console for debugging!
   console.log(tinderRequestCap, mambaRequestCap);
 } catch (err: any) {
   console.error(`Error caught in background.js: ${err.stack}`);
 }
+
+export {
+  tinderRequestCap,
+  mambaRequestCap,
+};
---
+++ src/background/messages/getImages.ts
@@ -0,0 +1,31 @@
+import type { PlasmoMessaging } from '@plasmohq/messaging';
+import { tinderRequestCap, mambaRequestCap } from '@/background/index';
+import { ImageType, Sites } from '@/misc/types';
+
+export type getImagesRequest = {
+	site: Sites;
+};
+
+export type getImagesResponse = {
+	images: ImageType[];
+};
+
+const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
+  // get the images from the main thread
+  let images = [];
+  switch (req.body.site) {
+    case Sites.TINDER:
+      images = tinderRequestCap.images;
+      break;
+    case Sites.MAMBA:
+      images = mambaRequestCap.images;
+      break;
+    default:
+      break;
+  }
+  res.send({
+    images,
+  });
+};
+
+export default handler;
