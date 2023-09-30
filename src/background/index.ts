6f03a88ac12b3e7a9e2035ef2a9b842d10b9925d
James <james@arnott.tech>
2023-09-30 14:31:23 +0100

    feat: added analytics + consent flags

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,10 +1,12 @@
 /* eslint-disable import/no-mutable-exports */
 import { Storage } from '@plasmohq/storage';
 import * as Sentry from '@sentry/browser';
+import browser from 'webextension-polyfill';
 import { debug, defaultSettings } from '@/misc/config';
 import { Sites } from '@/misc/types';
 import ImageRequestCapturer from './imageRequestCapturer';
 import { SENTRY_DSN } from './Misc';
+import { AnalyticsEvent } from '~src/misc/GA';
 
 const setDefaultSettings = async () => {
   const storage = new Storage();
@@ -14,15 +16,24 @@ const setDefaultSettings = async () => {
   }
 };
 
+const setupSentry = async () => {
+  const storage = new Storage({
+    area: 'sync',
+  });
+  const sentryConsent = await storage.get('sentryConsent');
+  if (sentryConsent.toLowerCase() === 'true') {
+    Sentry.init({
+      dsn: SENTRY_DSN,
+      integrations: [],
+    });
+  }
+};
+
 let tinderRequestCap: ImageRequestCapturer;
 let mambaRequestCap: ImageRequestCapturer;
 
 try {
-  Sentry.init({
-    dsn: SENTRY_DSN,
-    integrations: [],
-  });
-
+  setupSentry();
   setDefaultSettings();
 
   tinderRequestCap = new ImageRequestCapturer(['*://*.gotinder.com/*/*.jpg*', '*://*.gotinder.com/*/*.webp*', '*://*.gotinder.com/*/*.mp4*'], Sites.TINDER);
@@ -39,3 +50,25 @@ export {
   tinderRequestCap,
   mambaRequestCap,
 };
+
+/**
+ * When the user first installs the extension, open the main page
+ */
+browser.runtime.onInstalled.addListener(async (object) => {
+  if (chrome) {
+    if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+      const platform = await browser.runtime.getPlatformInfo();
+      AnalyticsEvent([
+        {
+          name: 'install',
+          params: {
+            platform: platform.os,
+          },
+        },
+      ]);
+    }
+  }
+
+  const consentUrl = browser.runtime.getURL('tabs/consent.html');
+  browser.tabs.create({ url: consentUrl });
+});
