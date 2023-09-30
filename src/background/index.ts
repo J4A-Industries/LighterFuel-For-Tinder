5e7a199b9b57c409e9225f8e9e9c3339c021a53f
James <james@arnott.tech>
2023-09-30 12:29:30 +0100

    feat: added sentry to background

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,9 +1,11 @@
 /* eslint-disable import/no-mutable-exports */
 import browser from 'webextension-polyfill';
 import { Storage } from '@plasmohq/storage';
+import * as Sentry from '@sentry/browser';
 import { debug, defaultSettings } from '@/misc/config';
 import { Sites } from '@/misc/types';
 import ImageRequestCapturer from './imageRequestCapturer';
+import { SENTRY_DSN } from './Misc';
 
 const setDefaultSettings = async () => {
   const storage = new Storage();
@@ -23,6 +25,16 @@ try {
 
   // prints the bg instance to the console for debugging!
   if (debug) console.log(tinderRequestCap, mambaRequestCap);
+
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    integrations: [
+      new Sentry.Replay(),
+    ],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
 } catch (err: any) {
   console.error(`Error caught in background.js: ${err.stack}`);
 }
