f360977b01392000e4b1cbd87515da118613e669
James <james@arnott.tech>
2023-10-10 19:42:00 +0100

    chore: moving tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  content: [
+    './src/popup/*.{jsx,tsx}',
+  ],
+  compilerOptions: {
+    baseUrl: 'src/',
+  },
+  theme: {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#d66b08',
+          secondary: '#7ae27a',
+          accent: '#fc4b8f',
+          neutral: '#1a1b28',
+          'base-100': '#1f272e',
+          info: '#356282',
+          success: '#0f853e',
+          warning: '#f6d013',
+          error: '#ed2662',
+        },
+      },
+    },
+  },
+  mode: 'jit',
+  plugins: [
+    require('daisyui'),
+    require('@tailwindcss/typography'),
+  ],
+  include: [
+    'src',
+    'types',
+  ],
+};
