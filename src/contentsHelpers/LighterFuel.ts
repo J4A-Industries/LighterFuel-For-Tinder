8bedcc8f97c42d7c9ca90e1990922c257e5d2693
James <james@arnott.tech>
2023-10-27 14:47:15 +0100

    feat: lighterfuel compatibility with new system

--- src/contentsHelpers/LighterFuel.ts
+++ src/contentsHelpers/LighterFuel.ts
@@ -3,6 +3,8 @@
 /* eslint-disable no-restricted-syntax */
 import browser from 'webextension-polyfill';
 
+import { Storage } from '@plasmohq/storage';
+import { sendToBackgroundViaRelay } from '@plasmohq/messaging';
 import {
   createButton,
   getTimeOld,
@@ -13,8 +15,7 @@ import {
   getDomsWithBGImages,
   getProfileImages,
 } from '@/contentsHelpers/Misc';
-
-import { debug, text } from '@/misc/config';
+import { debug, defaultSettings, text } from '@/misc/config';
 
 import {
   type ImageType,
@@ -25,8 +26,16 @@ import {
 } from '@/misc/types';
 
 import ImageHandler, { Events } from '@/contentsHelpers/ImageHandler';
+import type { photoInfo } from '~src/background/PeopleHandler';
+import type { getImageInfoRequest, getImageInfoResponse } from '~src/background/messages/getImageInfo';
 
-class LighterFuel extends ImageHandler {
+class LighterFuel {
+  showSettings: ShowSettings = defaultSettings;
+
+  storage: Storage;
+
+  site: Sites;
+
   profileSliderContainers: profileSliderContainer[];
 
   mainMutationObserver: MutationObserver;
@@ -36,20 +45,30 @@ class LighterFuel extends ImageHandler {
   shownProfileImages: Element[];
 
   constructor() {
-    super(Sites.TINDER); // Call the parent class constructor
-
     this.profileSliderContainers = [];
-
-    this.initialiseMessageListner = this.initialiseMessageListner.bind(this);
-
-    this.getData = this.getData.bind(this);
 
     this.startMonitorInterval();
   }
 
+  async getImageInfo(url: string) {
+    const res = await sendToBackgroundViaRelay<getImageInfoRequest, getImageInfoResponse>({
+      name: 'getImageInfo',
+      body: {
+        url,
+      },
+    });
+
+    return res.info;
+  }
+
   startMonitorInterval() {
-    setInterval(() => {
-      const keenSlider = [...document.querySelectorAll('div.keen-slider, div.profileCard__slider')];
+    setInterval(async () => {
+      let keenSlider = [...document.querySelectorAll('div.keen-slider, div.profileCard__slider')];
+
+      if (keenSlider.length === 2) {
+        keenSlider = [keenSlider[1]];
+      }
+
       if (keenSlider.length > 0) {
         // For every slider, make sure there's the overlay
         for (const slider of keenSlider) {
@@ -64,52 +83,53 @@ class LighterFuel extends ImageHandler {
             if (firstParent || secondParent) return curr;
             return acc;
           });
+
           const imageURL = getImageURLfromNode(currentImage);
+          console.log('currentImage', imageURL);
           if (!imageURL) {
             if (debug) {
               console.log('getImageURLfromNode returned undefined, skipping this image');
             }
             break;
           }
-          const imageRecord = this.images.find((image) => imageURL.includes(image.url));
 
-          // this doesn't seem to be problematic, it sometimes just doesn't find the image
-          if (!imageRecord) {
-            break;
-          }
+          this.getImageInfo(imageURL).then((info) => {
+            if (!info) {
+              return;
+            }
+            let existingOverlay = slider.parentNode.querySelector('p.overlayBox, p.topBox');
+            const sliderParent = slider.parentNode;
 
-          let existingOverlay = slider.parentNode.querySelector('p.overlayBox, p.topBox');
-          const sliderParent = slider.parentNode;
-
-          if (existingOverlay) {
-            if (existingOverlay.parentNode) {
-              const existingOverlayInCorrectPlace = (existingOverlay.parentNode as HTMLElement).classList.contains('keen-slider')
+            if (existingOverlay) {
+              if (existingOverlay.parentNode) {
+                const existingOverlayInCorrectPlace = (existingOverlay.parentNode as HTMLElement).classList.contains('keen-slider')
                 || (existingOverlay.parentNode as HTMLElement).classList.contains('tappable-view');
 
-              if (!existingOverlayInCorrectPlace) {
-                existingOverlay.parentNode.removeChild(existingOverlay);
-                existingOverlay = undefined;
-                if (debug) {
-                  console.log('Existing overlay not in correct place, removing it');
+                if (!existingOverlayInCorrectPlace) {
+                  existingOverlay.parentNode.removeChild(existingOverlay);
+                  existingOverlay = undefined;
+                  if (debug) {
+                    console.log('Existing overlay not in correct place, removing it');
+                  }
                 }
               }
             }
-          }
 
-          if (!existingOverlay) {
-            const overlay = this.createOverlayNode(imageRecord);
-            sliderParent.appendChild(overlay.overlayNode);
-            overlay.onPlaced();
-            consoleOut('Added overlay');
-          } else // check to see if the overlay 'aria-url' matches the current image
-            if (existingOverlay.getAttribute('aria-url') !== imageRecord.url) {
-            // if not, update the overlay
-              existingOverlay.parentNode.removeChild(existingOverlay);
-              const overlay = this.createOverlayNode(imageRecord);
+            if (!existingOverlay) {
+              const overlay = this.createOverlayNode(info);
               sliderParent.appendChild(overlay.overlayNode);
               overlay.onPlaced();
-              consoleOut('Updated overlay');
-            }
+              consoleOut('Added overlay');
+            } else // check to see if the overlay 'aria-url' matches the current image
+              if (existingOverlay.getAttribute('aria-url') !== info.original) {
+                // if not, update the overlay
+                existingOverlay.parentNode.removeChild(existingOverlay);
+                const overlay = this.createOverlayNode(info);
+                sliderParent.appendChild(overlay.overlayNode);
+                overlay.onPlaced();
+                consoleOut('Updated overlay');
+              }
+          });
         }
       }
     }, 50);
@@ -123,14 +143,17 @@ class LighterFuel extends ImageHandler {
    * @param data The data for the image (using the last modified date and url for reverse lookup)
     * @returns The element with an onPlaced method
    */
-  createOverlayNode(data: ImageType) {
-    const { lastModified } = data;
+  createOverlayNode(data: photoInfo) {
     const overlayNode = document.createElement('p');
-    const date = new Date(lastModified);
+    const date = new Date(data.accountCreated);
     const xOld = getTimeOld(date.getTime());
-    overlayNode.innerHTML = `${text.overlay.uploadedAt}: ${date.getHours()}:${date.getMinutes()} ${date.toLocaleDateString()} ${xOld} Old`;
-    overlayNode.appendChild(createButton(data.url));
-    overlayNode.setAttribute('aria-url', data.url);
+    overlayNode.innerHTML = `${text.overlay.createdAt}: ${date.getHours()}:${date.getMinutes()} ${date.toLocaleDateString()} ${xOld} Old`;
+    if (!data.hqUrl) {
+      console.log('no hqUrl', data);
+    }
+
+    overlayNode.appendChild(createButton(data.hqUrl));
+    overlayNode.setAttribute('aria-url', data.original);
     const onPlaced = () => {
       const bounds = overlayNode.getBoundingClientRect();
       // * whenever there's 100px above, we have room to place the box above
