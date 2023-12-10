a0c8d00c08e999364a3430a619e55b7c7e9483dc
James <james@arnott.tech>
2023-12-10 00:57:30 +0000

    refactor: removed bg event, the bt popup needs to be called from an onclick on the webpage

--- src/background/index.ts
+++ src/background/index.ts
@@ -43,8 +43,6 @@ const setupSentry = async () => {
 let mambaRequestCap: ImageRequestCapturer;
 let peopleHandler: PeopleHandler;
 
-const rubiksEventTarget = new EventTarget();
-
 try {
   setupSentry();
   setAndCheckDefaultSettings();
@@ -62,7 +60,6 @@ try {
 
 // exporting so the message handlers can access the images
 export {
-  rubiksEventTarget,
   peopleHandler,
   mambaRequestCap,
 };
