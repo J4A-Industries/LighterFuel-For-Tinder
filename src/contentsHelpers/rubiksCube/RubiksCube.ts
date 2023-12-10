1299363fb5a468f8133d5f50358d8b33b01b45af
James <james@arnott.tech>
2023-12-10 00:58:22 +0000

    feat: rubiks cube works

--- src/contentsHelpers/rubiksCube/RubiksCube.ts
+++ src/contentsHelpers/rubiksCube/RubiksCube.ts
@@ -1,9 +1,44 @@
 /* eslint-disable no-await-in-loop */
 /* eslint-disable class-methods-use-this */
-import { sendToBackground } from '@plasmohq/messaging';
-import type { activateRubiksCubeRequest, activateRubiksCubeResponse } from '~src/background/messages/activateRubiksCube';
-import { connectToBluetoothDevice, disconnectFromBluetoothDevice, startNotifications } from './bluetooth';
-import { decrypt, getMove, getState } from './decode';
+import {
+  connectToBluetoothDevice,
+  disconnectFromBluetoothDevice,
+  startNotifications,
+} from './bluetooth';
+import {
+  decrypt,
+  getMove,
+  getState,
+} from './decode';
+
+const changeImageViewed = (direction: 'next' | 'previous' | 'reset') => {
+  const tabList = [...document.querySelectorAll('div.CenterAlign')].filter((div) => div.getAttribute('role') === 'tablist');
+  if (!tabList.length) return;
+  tabList.forEach((list) => {
+    const selectedIndex = [...list.children].findIndex((div) => div.getAttribute('aria-selected') === 'true');
+    let nextIndex;
+
+    switch (direction) {
+      case 'next':
+        if (selectedIndex === -1 || selectedIndex + 1 >= list.children.length) return;
+        nextIndex = selectedIndex + 1;
+        break;
+      case 'previous':
+        if (selectedIndex === -1 || selectedIndex - 1 < 0) return;
+        nextIndex = selectedIndex - 1;
+        break;
+      case 'reset':
+        nextIndex = 0;
+        break;
+      default:
+        nextIndex = selectedIndex;
+    }
+
+    const next = list.children[nextIndex] as HTMLButtonElement;
+    if (!next) return;
+    next.click();
+  });
+};
 
 class RubiksCube {
   device: BluetoothDevice | undefined;
@@ -13,18 +48,25 @@ class RubiksCube {
   characteristic: BluetoothRemoteGATTCharacteristic | undefined;
 
   constructor() {
-    this.pollForActivation();
+    this.addButtonToPage();
   }
 
   handleMove(move: string) {
     console.log(`Move: ${move}`);
     const buttons = [...document.querySelectorAll('button.button')] as HTMLButtonElement[];
+    const tabList = [...document.querySelectorAll('div.CenterAlign')].filter((div) => div.getAttribute('role') === 'tablist');
     if (move === 'B') {
       // pass
       buttons[1].click();
+      changeImageViewed('reset');
     } else if (move === 'B\'') {
       // like
       buttons[3].click();
+      changeImageViewed('reset');
+    } else if (move === 'F') { // next image
+      changeImageViewed('next');
+    } else if (move === 'F\'') { // previous image
+      changeImageViewed('previous');
     }
   }
 
@@ -53,18 +95,37 @@ class RubiksCube {
     });
   }
 
-  async pollForActivation() {
-    while (!this.device) {
-      console.log('Polling for activation');
-      const res = await sendToBackground<activateRubiksCubeRequest, activateRubiksCubeResponse>({
-        name: 'activateRubiksCube',
+  addButtonToPage() {
+    // check for the profile page (where the buttons are)
+    setInterval(() => {
+      const menu = document.querySelector('.menu__content');
+      if (!menu) return;
+      const button = document.getElementById('rubiksCubeButton');
+      if (button) return;
+
+      const newElement = document.createElement('div');
+      newElement.className = 'Mt(20px)--ml Mt(16px)';
+      newElement.innerHTML += `
+      <div class="settings__container BdY Bdc($c-ds-divider-primary)" role="group">
+        <div class="menuItem Bgc($c-ds-background-primary) focus-visible_Bgc($c-ds-foreground-blue) Trsdu($fast)" id="rubiksCubeButton">
+            <div class="menuItem__contents Pos(r) Px(12px) Px(24px)--ml Py(0) M(0)--ml Mih(52px) settings__container_Px(16px) D(f) Jc(c) Fxd(c) W(100%) Cur(p) focus-background-style" role="button" tabindex="0">
+              <div class="D(f) Jc(sb) Ai(c)">
+                  <div class="Maw(80%) Ov(h) Tov(e) M(a) Cur(p)">Rubiks Cube Mode</div>
+              </div>
+            </div>
+        </div>
+      </div>`;
+      menu.appendChild(newElement);
+      const buttonElement = document.getElementById('rubiksCubeButton');
+      if (!buttonElement) return;
+      buttonElement.addEventListener('click', async () => {
+        try {
+          await this.handleActivation();
+        } catch (err) {
+          alert(`Error connecting to cube, try again. ${err}`);
+        }
       });
-      console.log('got response!', res);
-      if (res.active) {
-        console.log('Activating cube!');
-        await this.handleActivation();
-      }
-    }
+    }, 500);
   }
 }
 
