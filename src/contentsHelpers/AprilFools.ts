33bab3ae620c150e7461becf07dcbdda6fd6a373
James <james@arnott.tech>
2024-03-29 23:16:56 +0000

    chore: added date check

--- src/contentsHelpers/AprilFools.ts
+++ src/contentsHelpers/AprilFools.ts
@@ -4,6 +4,7 @@ import { sendToBackground, sendToBackgroundViaRelay } from '@plasmohq/messaging'
 import type { AprilFoolsReqRequest, AprilFoolsReqResponse } from '../background/messages/aprilFoolsRequest';
 import type { AprilFoolsRequest, AprilFoolsResponse } from '../background/messages/aprilFoolsSubmit';
 import { debug } from '../misc/config';
+import { checkDate } from './AprilFoolsMisc';
 
 const dislikeButtonPath = 'm15.44 12 4.768 4.708c1.056.977 1.056 2.441 0 3.499-.813 1.057-2.438 1.057-3.413 0L12 15.52l-4.713 4.605c-.975 1.058-2.438 1.058-3.495 0-1.056-.813-1.056-2.44 0-3.417L8.47 12 3.874 7.271c-1.138-.976-1.138-2.44 0-3.417a1.973 1.973 0 0 1 3.25 0L12 8.421l4.713-4.567c.975-1.139 2.438-1.139 3.413 0 1.057.814 1.057 2.44 0 3.417L15.44 12Z';
 
@@ -31,21 +32,22 @@ class AprilFools {
 
   constructor() {
     this.checkToRun();
-    document.addEventListener('DOMContentLoaded', () => {
-      const runInterval = setInterval(() => {
-				if(this.enabled){
-        	this.reRouteDislikeButton();
-        	this.monitorLeftSwipe();
-        	this.checkToRun();
-        	if (this.alreadyPassed) {
-         		clearInterval(runInterval);
-        	}
-				}
-      }, 50);
-    });
+    if (checkDate()) {
+      document.addEventListener('DOMContentLoaded', () => {
+        const runInterval = setInterval(() => {
+          this.reRouteDislikeButton();
+          this.monitorLeftSwipe();
+          this.checkToRun();
+          if (this.alreadyPassed) {
+            clearInterval(runInterval);
+          }
+        }, 50);
+      });
+    }
   }
 
   reRouteDislikeButton() {
+    if (!checkDate()) return;
     const innerSVG = [...document.querySelectorAll('path')].find((x) => x.getAttribute('d') === dislikeButtonPath);
     if (!innerSVG) return;
     const button = getParentElement(innerSVG, 'button');
