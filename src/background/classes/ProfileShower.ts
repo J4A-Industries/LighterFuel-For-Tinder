2ff3f7d0f10113ed28661df411e631e620f9eca9
Acorn221 <james@arnott.tech>
2024-12-27 17:28:13 +0000

    feat: handleAttemptedRejection and handleSwipe analytics calls created + created list of already shown profiles

--- src/background/classes/ProfileShower.ts
+++ src/background/classes/ProfileShower.ts
@@ -1,6 +1,8 @@
+/* eslint-disable no-underscore-dangle */
 import { Storage } from '@plasmohq/storage';
 
 import type { ShowProfilesFeatureFlag } from '~src/background/classes/FeatureFlagManager';
+import { AnalyticsEvent } from '~src/misc/GA';
 
 export type ProfileFeatureFlag = ShowProfilesFeatureFlag['profiles'][number];
 
@@ -12,8 +14,16 @@ export class ProfileShower {
     area: 'local',
   });
 
+  // The "flagId"s of the profiles that have been shown
+  // not using Tinder's internal ID incase the user gets banned
+  private shownProfileFlagIds: string[] | null = null;
+
   constructor(private profiles: ProfileFeatureFlag[], storage?: Storage) {
     if (storage) this.storage = storage;
+  }
+
+  async init() {
+    await this.getShownProfiles();
   }
 
   getProfile(): ProfileFeatureFlag | undefined {
@@ -21,16 +31,68 @@ export class ProfileShower {
       return undefined;
     }
 
-    return this.profiles.slice(0)[0];
+    const unshownProfile = this.profiles.find(
+      (profile) => !this.shownProfileFlagIds?.includes(profile.flagId),
+    );
+
+    return unshownProfile;
   }
 
-  handleSwipe(profileId: String, direction: 'left' | 'right') {
-    // TODO: analytics call
-    // TODO: marked as swiped - to not show the profile again
+  async handleSwipe(profileId: String, direction: 'left' | 'right') {
+    const flag = this.profiles.find(
+      (profile) => profile.webProfile.user._id === profileId,
+    );
+
+    if (!flag) {
+      throw new Error('Profile not found');
+    }
+
+    await AnalyticsEvent([
+      {
+        name: 'swipe',
+        params: {
+          direction,
+          profileId,
+          flagId: flag.flagId,
+        },
+      },
+    ]);
+    await this.markAsShown(flag.flagId);
   }
 
-  handleAttemptedRejection() {
+  async handleAttemptedRejection(profileId: string) {
     // TODO: analytics call
     // TODO: add to rejection count locally
+    const flag = this.profiles.find(
+      (profile) => profile.webProfile.user._id === profileId,
+    );
+
+    await AnalyticsEvent([
+      {
+        name: 'attemptedRejection',
+        params: {
+          profileId,
+          flagId: flag?.flagId,
+        },
+      },
+    ]);
+  }
+
+  async getShownProfiles() {
+    const shownProfiles = await this.storage.get<string[]>(
+      'shownProfileFlagIds',
+    );
+    if (shownProfiles) {
+      this.shownProfileFlagIds = shownProfiles;
+    }
+  }
+
+  async markAsShown(profileFlagId: string) {
+    if (!this.shownProfileFlagIds) {
+      this.shownProfileFlagIds = [];
+    }
+
+    this.shownProfileFlagIds.push(profileFlagId);
+    await this.storage.set('shownProfileFlagIds', this.shownProfileFlagIds);
   }
 }
